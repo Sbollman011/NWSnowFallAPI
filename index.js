bc2b955390704ec5a00ee68900058e50af0ead3f
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-01-07 13:48:58 -0800

    Snoq Season Total

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const app     = express();
 
 app.get('/', function(req, res){
     // The scraping magic will happen here
-    let url = "https://summitatsnoqualmie.com/conditions";
+    let urlSnoq = "https://summitatsnoqualmie.com/conditions";
+    let urlBaker ="";
+    let urlWhite ="";
+    let urlCrystal ="";
+    let urlSteven="";
 
     // The structure of our request call
     // The first parameter is our URL
     // The callback function takes 3 parameters, an error, response status code and the html
-    request(url, function(error, response, html) {
+    request(urlSnoq, function(error, response, html) {
         // First we'll check to make sure no errors occurred when making the request
         if (!error) {
         // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
@@ -19,14 +23,19 @@ app.get('/', function(req, res){
         // Finally, we'll define the variable we're going to capture
         // We'll be using Cheerio's function to single out the necessary information
         // using DOM selectors which are normally found in CSS.
-        var snoqSnowDepth24 = $('#conditions_area_d5ec042997234b3797b6799d47bfcadf > div > div > ul > li:nth-child(4) > div:nth-child(2) > ul > li:nth-child(2) > div > span')
+        var snoqSnow24API = $('#conditions_area_d5ec042997234b3797b6799d47bfcadf > div > div > ul > li:nth-child(4) > div:nth-child(2) > ul > li:nth-child(2) > div > span')
                                 .text().trim();
-        var snoqSnowDepth24List = snoqSnowDepth24.split(" ");
-        var snoqSnowDepth24 = snoqSnowDepth24[0];
+        snoqSnow24API.split(" ");
+        var snoqSnow24API = snoqSnow24API[0];
 
+        var snoqSnowSeasonAPI = $('#conditions_area_d5ec042997234b3797b6799d47bfcadf > div > div > ul > li:nth-child(4) > div:nth-child(2) > ul > li:nth-child(6) > div > span > span')
+                                .text().trim();
+        snoqSnowSeasonAPI.split(" ");
+        var snoqSnowSeasonAPI = snoqSnowSeasonAPI[0];
         // And now, the JSON format we are going to expose
         var json = {
-            snoqSnowDepth24 :snoqSnowDepth24,
+            snoqSnow24API :snoqSnow24API,
+            snoqSnowSeasonAPI : snoqSnowSeasonAPI
         };
 
         // Send the JSON as a response to the client
