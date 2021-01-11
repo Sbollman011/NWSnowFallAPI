7976c38b404720db5eae6e948d8cbeee12970a1e
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-01-10 21:14:25 -0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
 
 const app     = express();
 
@@ -20,10 +22,15 @@ app.get('/snoqualmie', function(req, res){
            var snoqSnowSeasonAPI = $('#conditions_area_d5ec042997234b3797b6799d47bfcadf > div > div > ul > li:nth-child(4) > div:nth-child(2) > ul > li:nth-child(6) > div > span > span')
                                     .text().trim();
             snoqSnowSeasonAPI.split(" ");
-        
+
+            var snoqWeatherTempAPI = $('#conditions_area_d5ec042997234b3797b6799d47bfcadf > div > div > ul > li:nth-child(4) > div:nth-child(2) > div > ul > li:nth-child(1) > div > span > span')
+            .text().trim();
+            snoqSnowSeasonAPI.split(" ");
+            
         var json = {
             snoqSnow24API :snoqSnow24API,
-            snoqSnowSeasonAPI : snoqSnowSeasonAPI
+            snoqSnowSeasonAPI : snoqSnowSeasonAPI,
+            snoqWeatherTempAPI : snoqWeatherTempAPI
         };
     
 
@@ -37,6 +44,7 @@ app.get('/snoqualmie', function(req, res){
 });
 
 app.get('/baker', function(req, res){
+
     // The scraping magic will happen here
     let url = "https://www.mtbaker.us/snow-report";
  
@@ -85,8 +93,13 @@ app.get('/baker', function(req, res){
     
            var whiteSnowSeasonAPI = $('#maincontent > section.weather-blocks > div > div:nth-child(2) > div > h2:nth-child(10)')
                                     .text().trim();
-                                    whiteSnowSeasonAPI = whiteSnowSeasonAPI.split('”');
+                                    whiteSnowSeasonAPI = whiteSnowSeasonAPI.split('summit');
+                                    whiteSnowSeasonAPI = whiteSnowSeasonAPI[1].split('”');
                                     whiteSnowSeasonAPI = whiteSnowSeasonAPI[0];
+
+                                    if(whiteSnow24API = "Trace"){
+                                        whiteSnow24API = "0";
+                                    }
             
             var json = {
                 whiteSnow24API :whiteSnow24API,
@@ -102,107 +115,106 @@ app.get('/baker', function(req, res){
 
 });
 
-/*
-app.get('/stevens', function(req, res){
-    // The scraping magic will happen here
-    let url = "https://www.crystalmountainresort.com/the-mountain/mountain-report-and-webcams#/";
- 
-
- 
-   request(url, function(error, response, html) {
-        if (!error) {
-        var $ = cheerio.load(html);
-        var whiteSnow24API = $('#main > div.grid-container.grid-container-padded > div > div.cell.medium-4.widget-area.hide-for-small-only > section:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2)')
-                                .text().trim();
-                                whiteSnow24API = whiteSnow24API.split("\"");
-                                whiteSnow24API = whiteSnow24API[0];
-
-       var whiteSnowSeasonAPI = $('#main-col > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div.col.conditions-block > div > p:nth-child(3) > strong')
-                                .text().trim();
-                                whiteSnowSeasonAPI = whiteSnowSeasonAPI.split(" ");
-                                whiteSnowSeasonAPI = whiteSnowSeasonAPI[0];
-        
-        var json = {
-            whiteSnow24API :whiteSnow24API,
-            whiteSnowSeasonAPI : whiteSnowSeasonAPI
-        };
-    
-
-        res.send(json);
-
-        }
-
-    });
-
-});
-
-  app.get('/stevens', function(req, res){
-    // The scraping magic will happen here
-    let url = "https://snoflo.org/report/snow/washington/stevens-pass--------/";
- 
-
- 
-   request(url, function(error, response, html) {
-      //  if (!error) {
-        var $ = cheerio.load(html);
-        var stevenSnow24API = $('#main > div.grid-container.grid-container-padded > div > div.cell.medium-4.widget-area.hide-for-small-only > section:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2)')
-                                .text()
-                               // stevenSnow24API = stevenSnow24API.split(" ");
-                                //stevenSnow24API = stevenSnow24API[0];
-
-       var stevenSnowSeasonAPI = $('#main > div.grid-container.grid-container-padded > div > div.cell.medium-4.widget-area.hide-for-small-only > section:nth-child(1) > table > tbody > tr:nth-child(6) > td:nth-child(2)')
-                                .text()
-                                //stevenSnowSeasonAPI = stevenSnowSeasonAPI.split(" ");
-                                //stevenSnowSeasonAPI = stevenSnowSeasonAPI[0];
-        
-        var json = {
-            stevenSnow24API :stevenSnow24API,
-            stevenSnowSeasonAPI : stevenSnowSeasonAPI
-        };
-    
-
-        res.send(json);
-
-      //  }
-
-    });
-
-});
 
 app.get('/crystal', function(req, res){
-    // The scraping magic will happen here
-    let url = "https://www.crystalmountainresort.com/the-mountain/mountain-report-and-webcams#/";
- 
-
- 
-   request(url, function(error, response, html) {
-        if (!error) {
-        var $ = cheerio.load(html);
-        var crystalSnow24API = $('#snowbase-cm-imperial')
-                                .text().trim()
-                               //crystalSnow24API = crystalSnow24API.split(" ");
-                               //crystalSnow24API = crystalSnow24API[0];
-
-       var crystalSnowSeasonAPI = $('#main-col > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div.col.conditions-block > div > p:nth-child(3) > strong')
-                                .text().trim()
-                                //crystalSnowSeasonAPI = crystalSnowSeasonAPI.split(" ");
-                                //crystalSnowSeasonAPI = crystalSnowSeasonAPI[0];
-        
-        var json = {
-            crystalSnow24API :crystalSnow24API,
-            crystalSnowSeasonAPI : crystalSnowSeasonAPI
-        };
     
+    const puppeteer = require('puppeteer');
+    const $ = require('cheerio');
+    // The scraping magic will happen here
+   
+const url = 'https://www.crystalmountainresort.com/the-mountain/mountain-report-and-webcams#/';
 
-        res.send(json);
-
-        }
-
+puppeteer
+  .launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  })
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      return page.content();
     });
+  })
+  .then(function(html) {
+   
+      var crystalSnowSeasonAPI= ($('#snowbase-cm-imperial:last',html).text());
+      var crystalSnow24API = ($('#main-col > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div.col.conditions-block > div > p:nth-child(3) > strong',html).text());
+      crystalSnow24API = crystalSnow24API.split(".");
 
-});*/
+      crystalSnow24API = crystalSnow24API[0];
+
+      var json = {
+        crystalSnow24API :crystalSnow24API,
+        crystalSnowSeasonAPI : crystalSnowSeasonAPI
+  
+    };
+
+
+    res.send(json);
+    
+  })
+  .catch(function(err) {
+    console.log("Stevens failed");
+  });
+
+});
+
+app.get('/stevens', function(req, res){
+    
+    const puppeteer = require('puppeteer');
+    const $ = require('cheerio');
+    // The scraping magic will happen here
+   
+const url = 'https://www.stevenspass.com/the-mountain/mountain-conditions/weather-report.aspx';
+
+puppeteer
+  .launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  })
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+   
+      var stevenSnowSeasonAPI= ($('#snow_report_1 > div.snow_report__content.row > ul > li:nth-child(6) > div > h5',html).text());
+      stevenSnowSeasonAPI = stevenSnowSeasonAPI.split("in");
+      stevenSnowSeasonAPI = stevenSnowSeasonAPI[0];
+      var stevenSnow24API = ($('#snow_report_1 > div.snow_report__content.row > ul > li:nth-child(3) > div > h5',html).text());
+      stevenSnow24API = stevenSnowSeasonAPI.split("in");
+      stevenSnow24API = stevenSnowSeasonAPI[0];
+
+
+
+      var json = {
+        stevenSnow24API :stevenSnow24API,
+        stevenSnowSeasonAPI : stevenSnowSeasonAPI
+  
+    };
+
+
+    res.send(json);
+    
+  })
+  .catch(function(err) {
+    console.log("Stevens failed");
+  });
+
+});
+
 
 app.listen((process.env.PORT || 5000));
 //app.listen('8080');
-console.log('API is running on http://localhost:8080');
+console.log('API is running on http://localhost:5000');
 module.exports = app;
