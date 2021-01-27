6e12fa84e71645a1f2204d9c52a2a8755778cfa1
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-01-26 19:57:25 -0800

    bestbuyapi

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ app.get('/bestbuy', function(req, res){
   
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
+      data = JSON.stringify(data);
       res.send(data);
     });
   });
